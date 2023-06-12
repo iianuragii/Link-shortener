649de2a3e3bd1a00cc065433bfd8a8bc7002e32e
Anurag Dutta <imanurag18@gmail.com>
2023-06-12 12:18:48 +0530

    changed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { connectMongo } = require('./connect');
 const urlRoute = require('./routers/url');
+const URL = require('./models/url')
 const app = express();
 const PORT = 8001;
 
@@ -11,5 +12,21 @@ connectMongo('mongodb://localhost:27017/short-url').then(() =>
 app.use(express.json());
 
 app.use("/url", urlRoute);
+app.use('/:shortId',async(req,res)=>{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+    {
+        shortId,
+    }, 
+    {
+        $push: {
+            visitHistory: {
+               timestamp: Date().now,
+            },
+        },
+    }
+    );
+    res.redirect(entry.redirectURL);
+});
 
 app.listen(PORT, () => console.log(`Server started at Port ${PORT}`));
